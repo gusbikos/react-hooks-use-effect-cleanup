1421aad72764b1e541ae3d37a8ac86450aaeb344
gusbikos <gusbikos@gmail.com>
2021-04-17 15:39:17 -0400

    useEffect-cleanup

--- src/Clock.js
+++ src/Clock.js
@@ -3,11 +3,28 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  // Still runs in background after you toggle clock off
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+  // }, []);
+
+
+  // Stops updating state this way after button is clicked.
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
+    const timerID = setInterval(() => {
+      setTime(new Date())
+      // console.log(timerID)
+      
     }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
+
 
   return <div>{time.toString()}</div>;
 }
